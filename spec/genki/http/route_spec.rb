9de052fc4df1edeb2e130d6f7df5fb898b1247d8
leonardoprg <leonardo.prg@gmail.com>
2016-02-21 23:39:10 +0000

    fixing rubocop

--- spec/genki/http/route_spec.rb
+++ spec/genki/http/route_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe Genki::Route do
-
   let(:route) { Genki::Route.new(:GET, '/') }
 
   it 'does has signature' do
-    expect(route.signature).to eql("GET/")
+    expect(route.signature).to eql('GET/')
   end
-
 end
